f0f47c02d672d11319995c2bac7f238bdc32ecff
mr-radio-store <mr.radio.pi314@gmail.com>
2026-05-31 16:23:59 +0700

    l298 error

---
+++ L298_DC_Servo.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+Servo s;
+
+void setup() {
+  Serial.begin(9600);
+  s.attach(45, 500, 2500);  // full pulse range
+}
+
+void loop() {
+  for (int angle = 0; angle <= 270; angle++) {
+    s.write(angle);
+    Serial.println(angle);
+    delay(20);
+  }
+
+  delay(2000);
+
+  for (int angle = 270; angle >= 0; angle--) {
+    s.write(angle);
+    Serial.println(angle);
+    delay(20);
+  }
+
+  delay(2000);
+}
